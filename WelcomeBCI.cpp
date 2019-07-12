cca3939a0eda14a83baf7eefcc8a315e730ea512
RTcY <52824468+LordPiers@users.noreply.github.com>
2019-07-12 15:28:49 +0300

    Create WelcomeBCI.cpp

---
+++ WelcomeBCI.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+	printf("************************************\n");
+	printf("WELCOME TO BUSINESS_CLASSGROUP INK\n");
+	printf("************************************\n");
+	
+	getch();
+	return 0;
+}
